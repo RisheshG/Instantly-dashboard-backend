e160f6ca1dfb56c07d2a8f0fd482b2722b896282
RisheshG <142112687+RisheshG@users.noreply.github.com>
2026-05-04 15:27:32 +0530

    Update API_KEY for server configuration

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const API_KEY = 'MWYzODNlMWYtYmM4YS00YjQ5LWExMDUtNzQ0MmZkMmRiODJhOkJWYVNzTVZ3VUxvRA==';
+const API_KEY = 'MWYzODNlMWYtYmM4YS00YjQ5LWExMDUtNzQ0MmZkMmRiODJhOnRldW5Ha3Rzc0h4bg==';
 
 // Middleware to verify Firebase ID token
 const verifyToken = async (req, res, next) => {
